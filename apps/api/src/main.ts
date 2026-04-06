b8fba6721cd41594120ca7d960670611c90a67d6
The Tran <nhanthe133@gmail.com>
2026-04-06 13:28:52 +0700

    Add class-transformer and class-validator dependencies; implement validation in CreateProductDto and enable global validation pipes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Tự động loại bỏ các trường không có trong DTO
+      forbidNonWhitelisted: true, // Báo lỗi nếu khách gửi thêm trường lạ
+      transform: true, // Tự động chuyển đổi kiểu dữ liệu nếu có thể
+    }),
+  );
   await app.listen(process.env.PORT ?? 3001);
   console.log(`API đang đợi lệnh tại: http://localhost:3001`);
 }
--- apps/api/src/products/dto/create-product.dto.ts
+++ apps/api/src/products/dto/create-product.dto.ts
@@ -1,11 +1,46 @@
 import { IProduct } from '@repo/shared';
+import {
+  IsString,
+  IsNumber,
+  IsArray,
+  IsUrl,
+  Min,
+  Max,
+  IsNotEmpty,
+} from 'class-validator';
 export class CreateProductDto implements Omit<IProduct, 'id'> {
+  @IsString({ message: 'Tên sản phẩm phải là chuỗi ký tự' })
+  @IsNotEmpty({ message: 'Tên sản phẩm không được để trống' })
   item_name!: string;
+
+  @IsString()
   category_id!: string;
+
+  @IsArray()
+  @IsUrl(
+    {},
+    { each: true, message: 'Mỗi hình ảnh phải là một đường link hợp lệ' },
+  )
   images!: string[];
+
+  @IsNumber()
+  @Min(0, { message: 'Giá không được nhỏ hơn 0' })
   price!: number;
+
+  @IsNumber()
+  @Min(0, { message: 'Số lượng không được nhỏ hơn 0' })
   quantity!: number;
+
+  @IsString()
   description!: string;
+
+  @IsNumber()
+  @Min(0)
+  @Max(5)
   rating!: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(100)
   discount!: number;
 }
